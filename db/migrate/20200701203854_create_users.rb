6d754239a3ebf39e38e5c586ce164b04b16162f3
agdsarge <agdsargeant@gmail.com>
2020-07-02 13:58:51 -0400

    first three models

---
+++ db/migrate/20200701203854_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :bio
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
