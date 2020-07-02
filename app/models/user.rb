feb00ef487cfda06f8ff59e1e0ce8532d34c54fc
agdsarge <agdsargeant@gmail.com>
2020-07-02 17:09:56 -0400

    jesse and i are authentication masters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :comments
     has_many :posts
     has_secure_password
+    validates :username, uniqueness: { case_sensitive: false }
     # has_many :messages
     # has_many :remarks
 end
