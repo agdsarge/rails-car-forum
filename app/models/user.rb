3d4e11977895c0d0d615b58b36adb4ef9fd3e85a
agdsarge <agdsargeant@gmail.com>
2020-07-02 14:58:42 -0400

    three models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :comments
+    has_many :posts
+    # has_many :messages
+    # has_many :remarks
 end
--- db/migrate/20200702175114_create_posts.rb
+++ db/migrate/20200702175114_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :body
+      t.text :body
       t.string :picture
       t.references :user, null: false, foreign_key: true
 
