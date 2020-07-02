8149fd231cfc2a9fb02e629064c752692cf2f036
jlangley3 <jlangleyiii@aol.com>
2020-07-02 18:35:05 -0400

    working on models

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -17,6 +17,6 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :password, :first_name, :last_name, :bio, :zip)
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :zip)
     end
 end
