370bd3096b9156013542e76d924e45343ed62ba2
agdsarge <agdsargeant@gmail.com>
2020-07-03 13:37:41 -0400

    some serialization for the posts index"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
     def index
-        render json: Post.all
+        render json: Post.all.to_json(include: {user: {only: [:username, :bio] }})
     end
 end
