3092348c4949048fcef37c2aa4974cbf5bed08ad
agdsarge <agdsargeant@gmail.com>
2020-07-08 15:05:36 -0400

    changed routes to be more RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :show, :create, :destroy, :update]
     resources :comments, only: [:index, :create, :destroy, :update]
     resources :messages, only: [:show]
-    resources :remarks, only: [:post]
+    resources :remarks, only: [:create]
 
     post '/messages/new', to: 'messages#search'
     get '/users/my_messages/:id', to: 'users#my_direct_messages'
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
             #get '/users/', to: 'users#index'
             post '/login', to: 'auth#create'
             get '/profile', to: 'users#profile'
-            get '/token', to: 'auth#jwt_log_in' 
+            get '/token', to: 'auth#jwt_log_in'
 
         end
     end
